231d1af80e78d6cafdcbed2f7d99469ec7828133
bunny-moba <bunnymoba265@gmail.com>
2025-06-08 19:20:00 +0530

    clean commented code

--- src/test/java/Demo_2/ecommers/Login.java
+++ src/test/java/Demo_2/ecommers/Login.java
@@ -13,23 +13,11 @@ public class Login extends BaseTest {
     @Test
     public void LoginTest() throws IOException {
 
-//        WebDriverManager.chromedriver().setup();
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://rahulshettyacademy.com/client");
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-//
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        driver.manage().window().maximize();
 
-          launchApp();
+        launchApp();
 
-        // driver.findElement(By.className("btn-primary")).click();
-//        driver.findElement(By.cssSelector("input[formcontrolname='userEmail']")).sendKeys("testng17@gmail.com");
-//        driver.findElement(By.cssSelector("input[formcontrolname='userPassword']")).sendKeys("Test@123");
-//        driver.findElement(By.id("login")).click();
-//
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.loginIn("testng17@gmail.com","Test@123");
+        loginPage.loginIn("testng17@gmail.com", "Test@123");
 
         String title = driver.getTitle();
 
